58273f74dffd4e1e00cb7068645b30c93cbae041
Karthikeyan Subramanian <46640177+kar12thik@users.noreply.github.com>
2020-10-17 21:29:07 +0530

    Gifs refactored using Custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./styles.css";
-import Random from "./components/Random_V1";
-import Tag from "./components/Tag_V1";
+// import Random from "./components/Random_V1";
+// import Tag from "./components/Tag_V1";
+import Random from "./components/Random_V2";
+import Tag from "./components/Tag_V2";
 
 export default function App() {
   return (
